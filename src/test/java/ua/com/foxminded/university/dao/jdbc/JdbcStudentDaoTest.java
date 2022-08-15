90f6c95a20ee1a1c41407b40c66d59c72f918b6a
Alexander <2951493@gmail.com>
2022-08-15 15:15:21 +0300

    JdbcTemplate bean has been reterned to the dao.xml file.

--- src/test/java/ua/com/foxminded/university/dao/jdbc/JdbcStudentDaoTest.java
+++ src/test/java/ua/com/foxminded/university/dao/jdbc/JdbcStudentDaoTest.java
@@ -1,38 +1,31 @@
 package ua.com.foxminded.university.dao.jdbc;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 
 import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import ua.com.foxminded.university.entity.StudentEntity;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("/test-config.xml")
 @ExtendWith(MockitoExtension.class)
 class JdbcStudentDaoTest {
     
+    private DataSource dataSource;
     
-  //  private Properties studentQueries;
-    private DataSource embeddedDatabase;
-    
-    public JdbcStudentDaoTest(
-           // Properties studentQueries, 
-            DataSource embeddedDatabase) {
-      //  this.studentQueries = studentQueries;
-        this.embeddedDatabase = embeddedDatabase;
+    public JdbcStudentDaoTest(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     @InjectMocks
@@ -54,8 +47,8 @@ class JdbcStudentDaoTest {
     
     @Test
     void getStudentById_GettingStudent_CorrectData() {
-        when(properties.get(anyString())).thenReturn("select * from university.students where id=?");
-        jdbcTemplate.setDataSource(embeddedDatabase);
+     //   when(properties.get(anyString())).thenReturn("select * from university.students where id=?");
+        jdbcTemplate.setDataSource(dataSource);
         StudentEntity student = jdbcStudentDao.getStudentById(1);
         System.out.println(student.toString());
         assertEquals('1', student.getId());
