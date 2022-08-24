0641836053298a8cb146bfab0474a14b534f8741
Alexander <2951493@gmail.com>
2022-08-24 12:49:36 +0300

    - The insert_InsertingStudentToDatabase_CorrectInsertedData method of the JdbcStudentDaoTest class has been complited.

--- src/test/java/ua/com/foxminded/university/dao/jdbc/JdbcStudentDaoTest.java
+++ src/test/java/ua/com/foxminded/university/dao/jdbc/JdbcStudentDaoTest.java
@@ -27,8 +27,10 @@ class JdbcStudentDaoTest {
     private static final String FIRST_NAME = "first_name";
     private static final String GROUP_ID = "group_id";
     private static final String TEST_INSERT = "test_insert";
-    private static final String LAST_NAME_STUDENT = "Smith";
-    private static final String FIRST_NAME_STUDENT = "Alex";
+    private static final String DATABASE_LAST_NAME_STUDENT = "Smith";
+    private static final String DATABASE_FIRST_NAME_STUDENT = "Alex";
+    private static final String LAST_NAME_STUDENT = "Deniels";
+    private static final String FIRST_NAME_STUDENT = "Jonh";
     private static final int GROUP_ID_NUMBER = 1;
     private static final int STUDENT_ID_NUMBER = 1;
     
@@ -38,6 +40,9 @@ class JdbcStudentDaoTest {
     
     @Autowired
     Properties studentQueries;
+    
+    @Autowired
+    Properties testStudentQueries;
     
     
 //    public int update(T entity);
@@ -53,10 +58,10 @@ class JdbcStudentDaoTest {
         student.setLastName(LAST_NAME_STUDENT);
         student.setGroup(new GroupEntity(GROUP_ID_NUMBER));
         studentDao.insert(student);
-        Map<String,Object> databaseStudent = jdbcTemplate.queryForMap(studentQueries
+        Map<String,Object> databaseStudent = jdbcTemplate.queryForMap(testStudentQueries
                 .getProperty(TEST_INSERT));
-        assertEquals(student.getFirstName(), databaseStudent.get(FIRST_NAME));
-        assertEquals(student.getLastName(), databaseStudent.get(LAST_NAME));
+        assertEquals(student.getFirstName(), databaseStudent.get(FIRST_NAME).toString());
+        assertEquals(student.getLastName(), databaseStudent.get(LAST_NAME).toString());
         assertEquals(student.getGroup().getId(), databaseStudent.get(GROUP_ID));
     }
     
@@ -66,8 +71,8 @@ class JdbcStudentDaoTest {
         StudentEntity student = studentDao.getById(STUDENT_ID_NUMBER);
         StudentEntity expectedResult = new StudentEntity();
         expectedResult.setId(STUDENT_ID_NUMBER);
-        expectedResult.setFirstName(FIRST_NAME_STUDENT);
-        expectedResult.setLastName(LAST_NAME_STUDENT);
+        expectedResult.setFirstName(DATABASE_FIRST_NAME_STUDENT);
+        expectedResult.setLastName(DATABASE_LAST_NAME_STUDENT);
         expectedResult.setGroup(new GroupEntity(GROUP_ID_NUMBER));
         assertEquals(expectedResult, student);
     }
