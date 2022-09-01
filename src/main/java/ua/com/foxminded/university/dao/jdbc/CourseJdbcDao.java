0999cf45a5058a5fed72c5d5e400ab47fbe8c502
Alexander <2951493@gmail.com>
2022-09-01 16:53:00 +0300

    - The getTimetableListByCourseId() method of the CourseJdbcDao class has been amended by using RowMapper class.

--- src/main/java/ua/com/foxminded/university/dao/jdbc/CourseJdbcDao.java
+++ src/main/java/ua/com/foxminded/university/dao/jdbc/CourseJdbcDao.java
@@ -1,7 +1,6 @@
 package ua.com.foxminded.university.dao.jdbc;
 
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -12,27 +11,15 @@ import org.springframework.stereotype.Repository;
 
 import ua.com.foxminded.university.dao.CourseDao;
 import ua.com.foxminded.university.dao.jdbc.mapper.CourseMapper;
+import ua.com.foxminded.university.dao.jdbc.mapper.TimetableMapper;
 import ua.com.foxminded.university.entity.CourseEntity;
-import ua.com.foxminded.university.entity.GroupEntity;
-import ua.com.foxminded.university.entity.TeacherEntity;
 import ua.com.foxminded.university.entity.TimetableEntity;
-import ua.com.foxminded.university.entity.WeekDayEntity;
 
 @Repository
 public class CourseJdbcDao implements CourseDao {
     
-    private static final String WEEK_DAY = "week_day";
-    private static final String TIMETABLE_DESCRIPTION = "timetable_description";
-    private static final String END_TIME = "end_time";
-    private static final String START_TIME = "start_time";
-    private static final String GROUP_ID = "group_id";
-    private static final String TIMETABLE_ID = "timetable_id";
     private static final String GET_TIMETABL_LIST_BY_COURSE_ID = "course.getTimetableListByCourseId";
     private static final String UPDATE = "course.update";
-    private static final String COURSE_DESCRIPTION = "description";
-    private static final String COURSE_NAME = "name";
-    private static final String TEACHER_ID = "teacher_id";
-    private static final String COURSE_ID = "id";
     private static final String GET_BY_ID = "course.getById";
     private static final String INSERT = "course.insert";
     private static final String DELETE_BY_ID = "course.deleteById";
@@ -48,15 +35,23 @@ public class CourseJdbcDao implements CourseDao {
     
     @Override
     public CourseEntity getTimetableListByCourseId(int id) {
-        
-        CourseEntity courseWithTimetableList = jdbcTemplate.query(
-                queries.getProperty(GET_TIMETABL_LIST_BY_COURSE_ID), 
-                preparedStatement -> preparedStatement.setInt(1, id),
-                resultSet -> {
-                    return getCourseWithTimetableList(resultSet);
-                });
-        return courseWithTimetableList;
-        
+        CourseEntity courseWhithTimetableList = jdbcTemplate.queryForObject(
+                queries.getProperty(GET_TIMETABL_LIST_BY_COURSE_ID),
+                (resultSet, rowNum) -> {
+                    CourseEntity course = null;
+                    if (course == null) {
+                        CourseMapper courseMapper = new CourseMapper();
+                        course = courseMapper.mapRow(resultSet, rowNum);
+                        course.setTimetableList(new ArrayList<>());
+                    }
+                    
+                    TimetableMapper timetableMapper = new TimetableMapper();
+                    TimetableEntity timetable = timetableMapper.mapRow(resultSet, id);
+                    course.getTimetableList().add(timetable);
+                    return course;
+                },
+                id);
+        return courseWhithTimetableList;
     }
     
     @Override
@@ -87,34 +82,6 @@ public class CourseJdbcDao implements CourseDao {
                                                                                      entity));
     }
     
-    private CourseEntity getCourseWithTimetableList(ResultSet resultSet) throws SQLException {
-        CourseEntity course = null;
-        TimetableEntity timetable = null;
-        
-        while(resultSet.next()) {
-            if(course == null) {
-                course = getCourseEntity(resultSet);
-                course.setTimetableList(new ArrayList<>());
-            } 
-                                                                
-            timetable = getTimetableEntity(resultSet);
-            course.getTimetableList().add(timetable);
-        }
-        return course;
-    }
-    
-    private TimetableEntity getTimetableEntity (ResultSet resultSet) throws SQLException {
-        TimetableEntity timetable = new TimetableEntity();
-        timetable.setId(resultSet.getInt(TIMETABLE_ID));
-        timetable.setGroup(new GroupEntity(resultSet.getInt(GROUP_ID)));
-        timetable.setCourse(new CourseEntity(resultSet.getInt(COURSE_ID)));
-        timetable.setStartTime(resultSet.getLong(START_TIME));
-        timetable.setEndTime(resultSet.getLong(END_TIME));
-        timetable.setDescription(resultSet.getString(TIMETABLE_DESCRIPTION));
-        timetable.setWeekDay(WeekDayEntity.valueOf(resultSet.getString(WEEK_DAY)));
-        return timetable;
-    }
-
     private PreparedStatement getPreparedStatementOfUpdate(PreparedStatement preparedStatement, 
                                                            CourseEntity entity) throws SQLException {
         preparedStatement.setObject(1, entity.getTeacher().getId()); 
@@ -122,15 +89,6 @@ public class CourseJdbcDao implements CourseDao {
         preparedStatement.setString(3, entity.getDescription());
         preparedStatement.setInt(4, entity.getId());
         return preparedStatement;
-    }
-    
-    private CourseEntity getCourseEntity(ResultSet resultSet) throws SQLException {
-        CourseEntity course = new CourseEntity();
-        course.setId(resultSet.getInt(COURSE_ID));
-        course.setTeacher(new TeacherEntity(resultSet.getInt(TEACHER_ID)));
-        course.setName(resultSet.getString(COURSE_NAME));
-        course.setDescription(resultSet.getString(COURSE_DESCRIPTION));
-        return course;
     }
     
     private PreparedStatement getPreparedStatementOfInsert(PreparedStatement preparedStatement, 
