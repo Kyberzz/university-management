1f82911919597e2dcc482c37e4953a6bccee8274
Alexander <2951493@gmail.com>
2022-09-01 17:09:56 +0300

    The CourseMapper has been corrected.

--- src/main/java/ua/com/foxminded/university/dao/jdbc/mapper/CourseMapper.java
+++ src/main/java/ua/com/foxminded/university/dao/jdbc/mapper/CourseMapper.java
@@ -2,15 +2,11 @@ package ua.com.foxminded.university.dao.jdbc.mapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import org.springframework.jdbc.core.RowMapper;
 
 import ua.com.foxminded.university.entity.CourseEntity;
 import ua.com.foxminded.university.entity.TeacherEntity;
-import ua.com.foxminded.university.entity.TimetableEntity;
 
 public final class CourseMapper implements RowMapper<CourseEntity> {
     
@@ -26,12 +22,6 @@ public final class CourseMapper implements RowMapper<CourseEntity> {
         course.setDescription(resultSet.getString(DESCRIPTION));
         course.setName(resultSet.getString(NAME));
         course.setTeacher(new TeacherEntity(resultSet.getInt(TEACHER_ID)));
-        
-        
-      //  TimetableMapper timetableMapper = new TimetableMapper();
-      //  TimetableEntity timetable = timetableMapper.mapRow(resultSet, rowNum);
-        
-      //  course.getTimetableList().add(timetable);    
         return course;
     }
 }
