82355f5c63f224eda25a4e601ee86b7452eb6d11
Alexander <477163@gmail.com>
2023-06-15 18:16:26 +0300

    The TeacherController was developed

--- src/main/java/ua/com/foxminded/university/controller/TeacherController.java
+++ src/main/java/ua/com/foxminded/university/controller/TeacherController.java
@@ -1,5 +1,7 @@
 package ua.com.foxminded.university.controller;
 
+import static ua.com.foxminded.university.controller.UserController.USERS_ATTRIBUTE;
+
 import java.util.List;
 
 import javax.validation.Valid;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 import ua.com.foxminded.university.comparator.LessonDTOComparator;
@@ -30,7 +33,7 @@ public class TeacherController extends DefaultController {
     
     public static final String TEACHER_TEMPLATE_PATH = "teachers/teacher";
     public static final int TEACHER_ID = 1;
-    public static final String TEACHERS_LIST_TEMPATE_PATH = "teachers/list";
+    public static final String TEACHERS_LIST_TEMLPATE_PATH = "teachers/list";
     public static final String TEACHERS_ATTRIBUTE = "teachers";
     public static final String TEACHER_ATTRIBUTE = "teacher";
 
@@ -38,36 +41,47 @@ public class TeacherController extends DefaultController {
     private final UserService userService;
     private final LessonService lessonService;
     
-    @GetMapping("/details/{teacherId}")
-    public String getDetails(@PathVariable int teacherId, Model model) {
+    @PostMapping("/{teacherId}/delete-lesson")
+    public String deleteTeacherLesson(@PathVariable int teacherId, 
+                                      @RequestParam int lessonId) {
+        lessonService.deleteById(lessonId);
+        
+        return new StringBuilder().append(REDIRECT_KEY_WORD)
+                                  .append(SLASH)
+                                  .append(TEACHER_TEMPLATE_PATH)
+                                  .append(SLASH)
+                                  .append(teacherId)
+                                  .toString();
+    }
+    
+    @GetMapping("/teacher/{teacherId}")
+    public String getById(@PathVariable int teacherId, Model model) {
         TeacherDTO teacher = teacherService.getById(teacherId);
         List<LessonDTO> lessons = lessonService.getLessonsByTeacherId(teacherId);
         lessonService.addLessonTiming(lessons);
         lessons.sort(new LessonDTOComparator());
+        List<UserDTO> users = userService.getAll();
         
+        model.addAttribute(USERS_ATTRIBUTE, users);
         model.addAttribute(LessonController.LESSONS_ATTRIBUTE, lessons);
         model.addAttribute(TEACHER_ATTRIBUTE, teacher);
         return TEACHER_TEMPLATE_PATH;
     }
     
-    @PostMapping("/update/{teacherId}")
-    public String update(@PathVariable int teacherId,
-                         @ModelAttribute @Valid TeacherDTO teacherDto) {
+    @PostMapping("/{teacherId}/update")
+    public String update(@PathVariable int teacherId, 
+                         @RequestParam int userId) {
         
-        TeacherDTO teacher = teacherService.getById(teacherId);
-        
-        if (teacherDto.getUser().hasEmail()) {
-            userService.updateEmail(teacher.getUser().getId(), 
-                                    teacherDto.getUser().getEmail());
-        }
-        
-        teacherDto.getUser().setId(teacher.getUser().getId());
-        userService.updateUserPerson(teacherDto.getUser());
+        TeacherDTO persistedTeacher = teacherService.getById(teacherId);
+        persistedTeacher.setUser(new UserDTO());
+        persistedTeacher.getUser().setId(userId);
+        teacherService.update(persistedTeacher);
         
         return new StringBuilder().append(REDIRECT_KEY_WORD)
                                   .append(SLASH)
-                                  .append(TEACHERS_LIST_TEMPATE_PATH)
-                                  .toString();
+                                  .append(TEACHER_TEMPLATE_PATH)
+                                  .append(SLASH)
+                                  .append(teacherId)                                  .toString();
     }
     
     @PostMapping("/delete/{teacherId}")
@@ -76,26 +90,20 @@ public class TeacherController extends DefaultController {
         
         return new StringBuilder().append(REDIRECT_KEY_WORD)
                                   .append(SLASH)
-                                  .append(TEACHERS_LIST_TEMPATE_PATH)
+                                  .append(TEACHERS_LIST_TEMLPATE_PATH)
                                   .toString();
     }
     
     @PostMapping("/create")
-    public String create(@ModelAttribute @Valid TeacherDTO teacher) {
-        
-        UserDTO user = userService.createUserPerson(teacher.getUser());
-        
-        if (teacher.getUser().hasEmail()) {
-            user = userService.updateEmail(user.getId(), 
-                                           teacher.getUser().getEmail());
-        } 
-        
-        teacher.setUser(user);
+    public String create(@RequestParam int userId) {
+        TeacherDTO teacher = TeacherDTO.builder()
+                                       .user(new UserDTO()).build();
+        teacher.getUser().setId(userId);
         teacherService.create(teacher);
         
         return new StringBuilder().append(REDIRECT_KEY_WORD)
                                   .append(SLASH)
-                                  .append(TEACHERS_LIST_TEMPATE_PATH)
+                                  .append(TEACHERS_LIST_TEMLPATE_PATH)
                                   .toString();
     }
 
@@ -104,8 +112,11 @@ public class TeacherController extends DefaultController {
         List<TeacherDTO> teachers = teacherService.getAll();
         teacherService.sortByLastName(teachers);
         TeacherDTO teacher = TeacherDTO.builder().build();
+        List<UserDTO> users = userService.getAll();
+        
+        model.addAttribute(USERS_ATTRIBUTE, users);
         model.addAttribute(TEACHER_ATTRIBUTE, teacher);
         model.addAttribute(TEACHERS_ATTRIBUTE, teachers);
-        return TEACHERS_LIST_TEMPATE_PATH;
+        return TEACHERS_LIST_TEMLPATE_PATH;
     }
 }
